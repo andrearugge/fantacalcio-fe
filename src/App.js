099b7f5a9c207a8ad562abd34c027bac7b37fd07
Andrea Ruggeri <hello@rggndr.it>
2024-09-05 16:21:57 +0200

    Reset auction

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import Home from "./components/Home";
 import TeamSetup from "./components/TeamSetup";
 import TeamView from "./components/TeamView";
 import TeamList from "./components/TeamList";
 import PlayerList from "./components/PlayerList";
 import CreateParticipant from "./components/CreateParticipant";
-import ParticipantList from "./components/ParticipantList";
+
 import "./index.css";
 
 function App() {
@@ -20,7 +21,6 @@ function App() {
           <Route path="/create-participant" element={<CreateParticipant />} />
           <Route path="/teams" element={<TeamList />} />
           <Route path="/list" element={<PlayerList />} />
-          <Route path="/participants" element={<ParticipantList />} />
         </Routes>
       </div>
     </Router>
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 function Home() {
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-4xl font-bold mb-4">Asta</h1>
-      <div className="flex space-x-3">
+      <h1 className="text-4xl font-bold mb-4">Asta Fantacalcio</h1>
+      <div className="flex flex-wrap gap-3">
         <Link
           to="/list"
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
@@ -18,6 +18,20 @@ function Home() {
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         >
           Squadre
+        </Link>
+
+        <Link
+          to="/auction-admin"
+          className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Gestione Asta (Admin)
+        </Link>
+
+        <Link
+          to="/auction-login"
+          className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Partecipa all'Asta
         </Link>
       </div>
     </div>
